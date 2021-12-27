305afc634ebd5a1ecdcea640d90e746977284150
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2021-12-27 11:15:16 -0500

    Functions

--- Functions/src/Functions.kt
+++ Functions/src/Functions.kt
@@ -1,19 +1,18 @@
 import java.util.*
 
 fun main() {
-
     helloWorld()
     printWithSpaces("KOTLIN")
     println(getCurrentDate())
-    println(max(18,82))
+    println(max(18, 82))
 }
 
-// no parameter, no return
+// no parameters, no return value
 fun helloWorld() {
     println("Hello world!")
 }
 
-// one parameter, no return
+// one parameter, no return value
 //"KOTLIN" -> "K O T L I N"
 fun printWithSpaces(text: String) {
     for (char in text) {
@@ -23,16 +22,16 @@ fun printWithSpaces(text: String) {
     println()
 }
 
-// no parameter, returns Date
+// no parameters, returns Date
 fun getCurrentDate(): Date {
     return Date()
 }
+// fun getCurrentDate() = Date()
 
 // two parameters, returns Int
 fun max(a: Int, b: Int): Int {
     if (a >= b) {
         return a
-
     } else {
         return b
     }
