75ddb85e0f162415f24e2e12c76ba7bbb8c2d243
sardaryanelina <87990387+sardaryanelina@users.noreply.github.com>
2022-03-23 23:07:48 -0400

    High order function/ Lambda/ Companion object/ Singleton

---
+++ HighOrderFun_Lambda_CompanionObject_Singleton_Project/src/CompanionObjectClass.kt
@@ -0,0 +1,10 @@
+class CompanionObjectClass {
+    companion object {
+        var companionObjectVariable = 0
+        fun companionObjectFun() = companionObjectVariable++
+    }
+
+    fun classFunction() {
+
+    }
+}
---
+++ HighOrderFun_Lambda_CompanionObject_Singleton_Project/src/SingletonObject.kt
@@ -0,0 +1,4 @@
+object SingletonObject {
+    var singletonVariable = 0
+    fun singletonFun() = singletonVariable++
+}
